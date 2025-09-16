584975af634cda121b67c36b16ce11d564949b35
Ashish269 <ashishkumar183183@gmail.com>
2025-09-17 00:49:23 +0530

    Add flights model and migration

--- src/models/airport.js
+++ src/models/airport.js
@@ -12,8 +12,14 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.belongsTo(models.City,{
         foreignKey: 'cityId',
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
+      });
+      this.hasMany(models.Flight,{
+        foreignKey: 'departureAirportId',
+        onDelete:'CASCADE'
+      });
+      this.hasMany(models.Flight,{
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
       })
     }
   }
