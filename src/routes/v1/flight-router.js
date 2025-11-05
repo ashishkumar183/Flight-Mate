2a66dbff891910737116f7138acdf9ecc4426252
Ashish269 <ashishkumar183183@gmail.com>
2025-11-05 15:14:19 +0530

    FEAT: Added filters for get all flights

--- src/routes/v1/flight-router.js
+++ src/routes/v1/flight-router.js
@@ -13,24 +13,9 @@ router.post('/',
     FlightMiddlewares.validateCreateRequest,
     FlightController.createFlight);
 
-// // /api/v1/airports GET
-// // Binds the GET request to the getAirports controller function to fetch all airports
-// router.get('/',
-//     FlightController.getAirports);
-
-// // /api/v1/airports/:id GET
-// // Binds the GET request to the getAirport controller function to fetch a specific airport by ID
-// router.get('/:id',
-//     FlightController.getAirport);
-
-// // /api/v1/airports/:id DELETE
-// // Binds the DELETE request to the destroyAirport controller function to delete an airport by ID
-// router.delete('/:id',
-//     FlightController.destroyAirport);
-
-// // /api/v1/airports/:id PATCH
-// // Binds the PATCH request to the updateAirport controller function to update an airport by ID
-// router.patch('/:id',
-//     FlightController.updateAirport);
+// /api/v1/flights?trips = MUM-DEL  GET    
+router.get('/',
+    FlightController.getAllFlights
+);
 
 module.exports = router;
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -2,6 +2,7 @@
 
 const { StatusCodes } = require('http-status-codes');
 const { FlightRepository } = require('../repositories');
+const {Op} = require('sequelize');
 const AppError = require('../utils/errors/app-error');
 
 const flightRepository = new FlightRepository();
@@ -22,7 +23,63 @@ async function createFlight(data) {
     }
 }
 
+async function getAllFlights(query) {
+    let customFilter = {};
+    let sortFilter = [];
+    // trips = MUM-DEL
+    // By GET request in params MUM-DEL we will get all MUM-DEL flights.
+    if(query.trips){
+        [departureAirportId,arrivalAirportId] = query.trips.split("-");
+        customFilter.departureAirportId = departureAirportId;
+        customFilter.arrivalAirportId = arrivalAirportId;
+        // TODO:- departure and arrival airport id should not be same
+    }
+
+    // Filter for price
+    if(query.price){
+        [minPrice,maxPrice] = query.price.split("-");
+        customFilter.price = {
+            [Op.between]: [minPrice,((maxPrice == undefined) ? 100000: maxPrice)]
+        }
+    }
+
+    // Total available Seats.
+    if(query.travellers){
+        customFilter.totalSeat = {
+            [Op.gte] : query.travellers
+        }
+    }
+
+    // Filters for particular date
+    if (query.tripDate) {
+        const startOfDay = new Date(query.tripDate);
+        startOfDay.setHours(0, 0, 0, 0);
+
+        const endOfDay = new Date(query.tripDate);
+        endOfDay.setHours(23, 59, 59, 999);
+
+        customFilter.departureTime = {
+            [Op.between]: [startOfDay, endOfDay]
+        };
+    }
+
+    // Sorting by departureTime
+    if(query.sort){
+        const params = query.sort.split(',');
+        const sortFilters = params.map((param) => param.split('_'));
+        sortFilter = sortFilters
+    }
+
+    try {
+        const flights = await flightRepository.getAllFlights(customFilter);
+        return flights;
+    } catch (error) {
+        throw new AppError('Cannot fetch data of all flights', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
 
 module.exports = {
-    createFlight
+    createFlight,getAllFlights
 };
